11ccbf0a23ec853c8ab0c56f72a8022446edd466
Dimitar Tagarev <dimitar45@tagarev.simplelogin.com>
2025-02-14 19:05:33 +0200

    working show argument but not working without argument

--- src/main.rs
+++ src/main.rs
@@ -2,42 +2,57 @@ mod app;
 mod pages;
 
 use anyhow::Result;
+use gdk::gio::ApplicationFlags;
 use gtk::prelude::*;
-use gtk::Application;
+use gtk::{Application};
+use gtk::glib::{Char, OptionArg, OptionFlags};
 use std::env;
+use std::fmt::Pointer;
 
 fn main() -> Result<()> {
-    let args: Vec<String> = env::args().collect();
-
     let application = Application::new(
         Some("com.app.holy-sheet"),
-        Default::default(),
+        ApplicationFlags::HANDLES_COMMAND_LINE,
+    );
+
+    // Add the --show option
+    application.add_main_option(
+        "show",
+    Char::from(b's'),
+        OptionFlags::IN_MAIN,
+        OptionArg::String,
+        "Show a markdown file",
+        None,
     );
 
     application.connect_activate(move |app| {
-        // Check if the --show argument is provided
-        // if args.len() > 1 && args[1] == "--show" {
-        //     if args.len() > 2 {
-        //         let file_name = &args[2];
-        //         println!("Filename: {:?}", file_name);
-                // if let Err(e) = pages::markdown_viewer::setup_markdown_viewer(app, "test.md") {
-                //     eprintln!("Error running markdown viewer: {:?}", e);
-                // }
-        //     } else {
-        //         eprintln!("Error: No filename provided for --show argument");
-        //     }
-        // } else {
-            // if let Err(e) = app::run_app_with_application(app) {
-            //     eprintln!("Error running app: {:?}", e);
-            // }
-        // }
+        // Default behavior when no command-line arguments are provided
+        if let Err(e) = app::run_app_with_application(app) {
+            eprintln!("Error running app: {:?}", e);
+        }
     });
 
-    if let Err(e) = pages::markdown_viewer::run_markdown_viewer("test.md") {
-        eprintln!("Error running markdown viewer: {:?}", e);
-    }
-    
-    // application.run();
+    application.connect_command_line(move |app, cmd_line| {
+        
+        // if args.contains(&std::ffi::OsString::from("--show")) {
+            if let Some(file_name) = cmd_line.options_dict().lookup_value("show", None) {
+                println!("Filename: {:?}", file_name.to_string().trim_matches('\''));
+                if let Err(e) = pages::markdown_viewer::setup_markdown_viewer(app, 
+                    &(file_name.to_string().trim_matches('\'').to_string() + ".md")) {
+                    eprintln!("Error running markdown viewer: {:?}", e);
+                }
+                return 0;
+            } else {
+                eprintln!("Error: No filename provided for --show argument");
+                return 1;
+            }
+        // }
+
+        // app.activate();
+        // 0
+    });
+
+    application.run();
 
     Ok(())
 }
