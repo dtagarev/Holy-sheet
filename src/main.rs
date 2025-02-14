0e65e4ddb401ef807eafa75461da845aa98c986f
Dimitar Tagarev <dimitar45@tagarev.simplelogin.com>
2025-02-14 05:58:49 +0200

    clap commnd

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,29 @@
 mod app;
 mod pages;
-// mod web_view_mode;
+
 use anyhow::Result;
-use clap::{Arg, ArgAction, Command};
+use clap::{Parser};
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Name of the cheatsheet to load
+    #[arg(long)]
+    show: Option<String>,
+}
 
 fn main() -> Result<()> {
-    // Просто стартираме логиката от app.rs
-    // let matches = Command::new("holysheet")
-    //     .version("0.1.0")
-    //     .about("Holy Sheet - GTK4 Cheatsheet helper")
-    //     .arg(
-    //         Arg::new("show")
-    //             .long("show")
-    //             .help("Name of the cheatsheet to load")
-    //             .value_name("NAME")
-    //             // Заявяваме, че този аргумент приема точно 1 стойност
-    //             .num_args(1)
-    //             // Искаме да сетне тази стойност (вместо да е true/false флаг)
-    //             .action(ArgAction::Set)
-    //     )
-    //     .get_matches();
+    let args = Args::parse();
 
-    // // Извличаме стойността на --show (ако е подадена)
-    // let show_what = matches.get_one::<String>("show").map(|s| s.as_str());
+    // Check if the --show argument is provided
+    if let Some(file_name) = args.show {
+        println!("Filename: {:?}", file_name);
+        if let Err(e) = pages::markdown_viewer::run_markdown_viewer(&file_name) {
+            eprintln!("Error running markdown viewer: {:?}", e);
+        }
+    } else {
+        app::run_app()?;
+    }
 
-    // web_view_mode::run_webview_mode("test.md");
-    pages::markdown_viewer::run_markdown_viewer("test.md");
-    app::run_app()
+    Ok(())
 }
